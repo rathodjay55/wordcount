ef87a330c10a2e89beef551551c69bd521c1299a
rathodjay55 <rathodjay55@yahoo.com>
2019-03-22 17:21:41 +0530

    second commit

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -5,8 +5,7 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home') 
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
@@ -19,7 +18,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
+    path('',views.homepage,name='home'),
     path('count/',views.count),
     path('about/',views.about,name='about'),
    
